85866695d68061e330cd54d78c58ccc49261848b
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2012-12-29 11:32:57 +0100

    indentation

--- test/matsu/sparql_test.clj
+++ test/matsu/sparql_test.clj
@@ -29,33 +29,37 @@
 
 (fact
   "fn query: ASK query form is supported"
-  (query (ask)
-         (where :s :p :o \.)) => "ASK WHERE { ?s ?p ?o . }"
+  (query
+    (ask)
+    (where :s :p :o \.)) => "ASK WHERE { ?s ?p ?o . }"
   )
 
 (fact
   "example query from Sparql spec"
-  (query (select :title)
-         (where (URI. "http://example.org/book/book1") (URI. "http://purl.org/dc/elements/1.1/title") :title \.))
-   => "SELECT ?title WHERE { <http://example.org/book/book1> <http://purl.org/dc/elements/1.1/title> ?title . }"
-)
+  (query
+    (select :title)
+    (where (URI. "http://example.org/book/book1") (URI. "http://purl.org/dc/elements/1.1/title") :title \.))
+  => "SELECT ?title WHERE { <http://example.org/book/book1> <http://purl.org/dc/elements/1.1/title> ?title . }"
+  )
 
 (fact
   "example2 query from sparql spec"
-  (query (with-prefixes :foaf)
-         (select :name :mbox)
-         (where :x [:foaf "name"] :name \.
-                :x [:foaf "mbox"] :mbox))
+  (query
+    (with-prefixes :foaf)
+    (select :name :mbox)
+    (where :x [:foaf "name"] :name \.
+           :x [:foaf "mbox"] :mbox))
   => "PREFIX foaf: <http://xmlns.com/foaf/0.1/> SELECT ?name ?mbox WHERE { ?x foaf:name ?name . ?x foaf:mbox ?mbox }"
   )
 
 (fact
-  (query (with-prefixes :foaf)
-         (ask)
-         (where :person \a [:foaf "Person"]
-                \; [:foaf "mbox"] (URI. "mailto:petter@petter.com") \.))
+  (query
+    (with-prefixes :foaf)
+    (ask)
+    (where :person \a [:foaf "Person"]
+           \; [:foaf "mbox"] (URI. "mailto:petter@petter.com") \.))
   =>
   "PREFIX foaf: <http://xmlns.com/foaf/0.1/> ASK WHERE { ?person a foaf:Person ; foaf:mbox <mailto:petter@petter.com> . }"
-)
+  )
 
-  ;The SPARQL keyword a is a shortcut for the common predicate rdf:type, giving the class of a resource.
+;The SPARQL keyword a is a shortcut for the common predicate rdf:type, giving the class of a resource.
