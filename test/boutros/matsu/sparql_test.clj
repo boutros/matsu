9d653d32e28dc575eead8a66d8c3181c42b45f2b
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2013-01-02 05:15:46 +0100

    use clojure.test instead of midje

--- test/boutros/matsu/sparql_test.clj
+++ test/boutros/matsu/sparql_test.clj
@@ -1,61 +1,59 @@
 (ns boutros.matsu.sparql-test
-  (:use midje.sweet
+  (:use clojure.test
         boutros.matsu.sparql)
   (:import (java.net URI)))
 
-(facts
-  "fn encode: correctily encodes RDF variables and literals"
-  (encode \*) => \*
-  (encode :keyword) => "?keyword"
-  (encode 23) => "\"23\"^^xsd:integer"
-  (encode 9.9) => "\"9.9\"^^xsd:decimal"
-  (encode "string") => "\"string\""
-  (encode true) => "\"true\"^^xsd:boolean"
-  (encode false) => "\"false\"^^xsd:boolean"
-  ;(encode (java.util.Date.)) => "not implemented!"
-  )
+; Utils
 
-; Testing the macros
+(testing "encode"
+  (are [a b] (= (encode a) b)
+       \*                          \*
+       :keyword                    "?keyword"
+       23                          "\"23\"^^xsd:integer"
+       9.9                         "\"9.9\"^^xsd:decimal"
+       "string"                    "\"string\""
+       true                        "\"true\"^^xsd:boolean"
+       false                       "\"false\"^^xsd:boolean"
+       (URI. "http://dbpedia.org") "<http://dbpedia.org>"
+       [:foaf "mbox"]              "foaf:mbox"))
 
-; (fact
-;   "query macro can be used with or without a saved query-map"
-;   (defquery my-query (select :s)) => truthy
-;   (query my-query
-;          (where : :p :o \.)) =>
-;   (query (select :s)
-;          (where :s :p :o \.))))
+; Macros
 
-(fact
-  "fn query: ASK query form is supported"
-  (query
-    (ask)
-    (where :s :p :o \.)) => "ASK WHERE { ?s ?p ?o . }"
-  )
 
-(fact
-  "example query from Sparql spec"
-  (query
-    (select :title)
-    (where (URI. "http://example.org/book/book1") (URI. "http://purl.org/dc/elements/1.1/title") :title \.))
-  => "SELECT ?title WHERE { <http://example.org/book/book1> <http://purl.org/dc/elements/1.1/title> ?title . }"
-  )
 
-(fact
-  "example2 query from sparql spec"
-  (query
+; Query DSL
+
+(testing "ask"
+  (is
+    (query
+      (ask)
+      (where :s :p :o \.))
+
+    "ASK WHERE { ?s ?p ?o . }"))
+
+(testing "select"
+  (is
+    (query
+      (select :s)
+      (where :s :p :o))
+
+    "SELECT ?s WHERE { :s :p :o }"))
+
+(testing "prefix and namespaced qualifiers"
+  (is
+    (query
     (prefix :foaf)
     (select :name :mbox)
     (where :x [:foaf "name"] :name \.
            :x [:foaf "mbox"] :mbox))
-  => "PREFIX foaf: <http://xmlns.com/foaf/0.1/> SELECT ?name ?mbox WHERE { ?x foaf:name ?name . ?x foaf:mbox ?mbox }"
-  )
 
-(fact
-  (query
+    "PREFIX foaf: <http://xmlns.com/foaf/0.1/> SELECT ?name ?mbox WHERE { ?x foaf:name ?name . ?x foaf:mbox ?mbox }")
+
+  (is
+    (query
     (prefix :foaf)
     (ask)
     (where :person \a [:foaf "Person"]
            \; [:foaf "mbox"] (URI. "mailto:petter@petter.com") \.))
-  =>
-  "PREFIX foaf: <http://xmlns.com/foaf/0.1/> ASK WHERE { ?person a foaf:Person ; foaf:mbox <mailto:petter@petter.com> . }"
-  )
+
+    "PREFIX foaf: <http://xmlns.com/foaf/0.1/> ASK WHERE { ?person a foaf:Person ; foaf:mbox <mailto:petter@petter.com> . }"))
