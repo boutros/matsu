f9c6f63048def1e1b2765f9fa741a24af26a72b7
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2013-01-11 09:35:35 +0100

    clean up project file

--- project.clj
+++ project.clj
@@ -1,10 +1,6 @@
 (defproject matsu "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "SPARQL query DSL"
+  :url "https://github.com/boutros/matsu"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :profiles {:dev {:dependencies [[midje "1.4.0"]
-                                  [com.stuartsierra/lazytest "1.2.3"]]}}
-  :repositories {"stuart" "http://stuartsierra.com/maven2"}
-  :dependencies [[org.clojure/clojure "1.4.0"]]
-  :main boutros.matsu.sparql)
+  :dependencies [[org.clojure/clojure "1.4.0"]])
