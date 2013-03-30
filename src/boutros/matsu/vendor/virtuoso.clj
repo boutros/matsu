8a25dcdee02b3e674f5edf4b5945c7ad32fd91aa
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2013-03-30 09:14:57 +0100

    add virtuoso SPARUL syntax fns for INSERT DATA & DELETE FROM

--- src/boutros/matsu/vendor/virtuoso.clj
+++ src/boutros/matsu/vendor/virtuoso.clj
@@ -3,9 +3,20 @@
   Virtuoso.
 
   Some info on the sql: and bif: namespaced functions here:
-  http://docs.openlinksw.com/virtuoso/rdfsparql.html#rdfsqlfromsparql")
+  http://docs.openlinksw.com/virtuoso/rdfsparql.html#rdfsqlfromsparql"
+  (:require [boutros.matsu.compiler :refer [encode]]))
 
 (defn modify
   "Virtuoso-specific equivalent of WITH."
   [q uri]
   (assoc q :with {:tag "MODIFY" :bounds [" "] :sep " " :content [uri]}))
+
+(defn delete-from [q g & more]
+  "Older SPARUL syntax: DELETE FROM <graph> { patterns... }"
+  (assoc q :delete {:tag (str "DELETE FROM " (encode g)) :bounds [" { " " } "]
+                    :sep " " :content (vec more)}))
+
+(defn insert-into [q g & more]
+  "Older SPARUL syntax: INSERT INTO <graph> { patterns... }"
+  (assoc q :insert {:tag (str "INSERT INTO " (encode g)) :bounds [" { " " } "]
+                    :sep " " :content (vec more)}))
