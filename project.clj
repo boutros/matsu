12c725257e250def9b8d9348c02c66f9e784cd6b
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2013-04-02 05:36:54 +0200

    update dependencies

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "https://github.com/boutros/matsu"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0"]
-                 [clj-time "0.4.5"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [clj-time "0.5.0"]])
